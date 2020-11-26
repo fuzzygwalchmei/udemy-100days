8e57c333e53cc7881618b315ecfa008486ee8d6c
fuzzygwalchmei <marc.falzon@gmail.com>
2020-11-26 16:34:04 +1100

    Udemy 100 Days of Code - Day 1

---
+++ 01.py
@@ -0,0 +1,39 @@
+#! /bin/python3
+
+# print('Hello World!')
+# Part 1
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to say')")
+print("print(\"what to say\")")
+
+print("Hello World!\n"*3)
+print("Hello","Marc")
+
+
+# Part 2
+#First *fork* your copy. Then copy-paste your code below this line 👇
+#Finally click "Run" to execute the tests
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the \"+\" sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+print("Hello "+input("What is your name?: "))
+
+
+# Part 3
+name = input("Enter your name: ")
+name_len = len(name)
+print(f"Your name {name} is {name_len} characters long")
+
+
+a = input("a: ")
+b = input("b: ")
+print(f"a == {a}, and b == {b}")
+b, a = a, b
+print("switcheroo...")
+print(f"Now... a == {a}, and b == {b}")
+
+
