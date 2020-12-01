9d73f2182b6dfc4d6f9492d7b801df044f6059b1
fuzzygwalchmei <marc.falzon@gmail.com>
2020-12-01 17:33:32 +1100

    udemy-100days of code day 6 ex1

---
+++ 06-exercises.py
@@ -0,0 +1,20 @@
+#! /bin/python3
+
+# Exercise 1 - Hurdles Loop
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def do_hurdle():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while True:
+    do_hurdle()
