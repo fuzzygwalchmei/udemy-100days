0c14afb71d025911b919f6edd37b356dd37e187e
fuzzygwalchmei <marc.falzon@gmail.com>
2020-12-03 17:52:12 +1100

    udemy 100 days of code day 8 ex 2 - caeser

--- 08-exercises.py
+++ 08-exercises.py
@@ -1,15 +1,25 @@
 #! /bin/python3
 
-# Exercise 1 - Paint Area Colour
-import math
-def paint_calc(height, width, cover):
-    tins = math.ceil(height*width/cover)
-    print(f"You will need {tins} tins of paint")
+# # Exercise 1 - Paint Area Colour
+# import math
+# def paint_calc(height, width, cover):
+#     tins = math.ceil(height*width/cover)
+#     print(f"You will need {tins} tins of paint")
 
 
-test_h = int(input("Height of wall: "))
-test_w = int(input("Width of wall: "))
+# test_h = int(input("Height of wall: "))
+# test_w = int(input("Width of wall: "))
 
-coverage = 5
+# coverage = 5
 
-paint_calc(height = test_h, width = test_w, cover = coverage)
+# paint_calc(height = test_h, width = test_w, cover = coverage)
+
+# # Exercise 2 - Prime number checker
+# def prime_chcker(number: int):
+#     is_prime = all([number%x != 0 for x in range(2, number//2)])
+#     print("Yes, its Prime" if is_prime else "No, Its not Prime")
+
+# n = int(input("Check this number: "))
+# prime_chcker(number=n)
+
+# Caeser Cipher Encryption and Decryption in scratching post
