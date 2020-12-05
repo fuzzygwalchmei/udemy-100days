9f9c09eb6a277234a912fca85c972641b0553f41
fuzzygwalchmei <marc.falzon@gmail.com>
2020-12-05 19:27:45 +1100

    udemy 100 days of code - day 10 ex 2

--- 10-exercises.py
+++ 10-exercises.py
@@ -6,25 +6,52 @@
 
 # print(format_name("MaRC", "falZON"))
 
-def is_leap_year(year):
-    if year%400 == 0:
-        return True
-    elif year%100 ==0:
-        return False
-    elif year%4==0:
-        return True
-    else:
-        return False
+# def is_leap_year(year):
+#     if year%400 == 0:
+#         return True
+#     elif year%100 ==0:
+#         return False
+#     elif year%4==0:
+#         return True
+#     else:
+#         return False
 
-def days_in_month(year, month):
-    month_days = [31,28,31,30,31,30,31,31,30,31,30,31]
-    days = month_days[month-1]
-    if is_leap_year(year) and month ==2:
-        days +=1
-    return days
+# def days_in_month(year, month):
+#     month_days = [31,28,31,30,31,30,31,31,30,31,30,31]
+#     days = month_days[month-1]
+#     if is_leap_year(year) and month ==2:
+#         days +=1
+#     return days
 
 
-year = int(input("Enter a year: "))
-month = int(input("Enter a month: "))
-days = days_in_month(year, month)
-print(days)
+# year = int(input("Enter a year: "))
+# month = int(input("Enter a month: "))
+# days = days_in_month(year, month)
+# print(days)
+
+# Exercise 2 - Calculator
+
+def add(a, b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mult(a,b):
+    return a*b
+
+def div(a,b):
+    return a//b
+repeat = "y"
+func = {"+":add, "-":sub, "*":mult, "/":div}
+
+result = int(input("What is the first value: "))
+while repeat == "y":
+    a = result
+    op = func.get(input("Which operator (+,-,*,/): "),add)
+    b = int(input("What is the second number: "))
+    result = op(a,b)
+    print(result)
+    repeat = input("Do you want to more? (y/n): ").lower()
+
+    
