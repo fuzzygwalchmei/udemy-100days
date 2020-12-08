f111aca52cc8aefe9a37a80d8de7d97cdb66585f
fuzzygwalchmei <marc.falzon@gmail.com>
2020-12-08 17:36:30 +1100

    udemy 100 days of code day 13 ex 3

--- 13-exercises.py
+++ 13-exercises.py
@@ -10,22 +10,29 @@
 #     print("This is an odd number")
 
 
-# Exercise 2 - Debugging Leap Year
-year = int(input("Which year do you want to check?: "))
+# # Exercise 2 - Debugging Leap Year
+# year = int(input("Which year do you want to check?: "))
 
-if year%4 == 0:
-    if year%100==0:
-        if year%400==0:
-            print("Leap Year")
-        else:
-            print("Not Leap Year")
-    else:
-        print("Leap Year")
-else:
-    print("Not Leap Year")
-
-
+# if year%4 == 0:
+#     if year%100==0:
+#         if year%400==0:
+#             print("Leap Year")
+#         else:
+#             print("Not Leap Year")
+#     else:
+#         print("Leap Year")
+# else:
+#     print("Not Leap Year")
 
 
 #Exercise 3 - Debugging FizzBuzz
 
+for number in range(1,101):
+    if number%3==0 and number%5==0:
+        print("Fizzbuzz")
+    elif number%3==0:
+        print("Fizz")
+    elif number%5==0:
+        print("Buzz")
+    else:
+        print(number)
