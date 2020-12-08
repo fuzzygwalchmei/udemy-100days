1d10df142c357d8f0e5940b9b06bcaf089922cf5
fuzzygwalchmei <marc.falzon@gmail.com>
2020-12-08 17:29:42 +1100

    udemy 100 days of code day 13 ex 1

---
+++ 13-exercises.py
@@ -0,0 +1,20 @@
+#! /bin/python3
+
+# Exercise 1 - Debugging Odd or Even
+
+number = int(input("Which number do you want to check?: "))
+
+if number%2 == 0:
+    print("This is an even number")
+else:
+    print("This is an odd number")
+
+
+# Exercise 2 - Debugging Leap Year
+
+
+
+
+
+#Exercise 3 - Debugging FizzBuzz
+
