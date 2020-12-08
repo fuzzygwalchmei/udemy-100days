17a05aa8928bb874f57a008dc63fbd9f3f1edd38
fuzzygwalchmei <marc.falzon@gmail.com>
2020-12-08 17:32:47 +1100

    udemy 100 days of code day 13 ex 2

--- 13-exercises.py
+++ 13-exercises.py
@@ -1,17 +1,28 @@
 #! /bin/python3
 
-# Exercise 1 - Debugging Odd or Even
+# # Exercise 1 - Debugging Odd or Even
 
-number = int(input("Which number do you want to check?: "))
+# number = int(input("Which number do you want to check?: "))
 
-if number%2 == 0:
-    print("This is an even number")
-else:
-    print("This is an odd number")
+# if number%2 == 0:
+#     print("This is an even number")
+# else:
+#     print("This is an odd number")
 
 
 # Exercise 2 - Debugging Leap Year
+year = int(input("Which year do you want to check?: "))
 
+if year%4 == 0:
+    if year%100==0:
+        if year%400==0:
+            print("Leap Year")
+        else:
+            print("Not Leap Year")
+    else:
+        print("Leap Year")
+else:
+    print("Not Leap Year")
 
 
 
