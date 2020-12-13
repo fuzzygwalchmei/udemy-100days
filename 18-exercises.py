a81c54d32dc9a740c497540c811b2ff7a06531d8
fuzzygwalchmei <marc.falzon@gmail.com>
2020-12-13 14:55:26 +1100

    udemy 100 days of code day 18 completed.

--- 18-exercises.py
+++ 18-exercises.py
@@ -2,17 +2,23 @@
 # 
 from random import choice, randint
 from turtle import Turtle, Screen, colormode
+import colorgram
 
 directions = [0, 90, 180, 270]
+
+
+colors = colorgram.extract("hirst.jpeg",20)
+rgb_colors = [(c.rgb.r, c.rgb.b, c.rgb.g) for c in colors]
+
 
 timmy = Turtle()
 colormode(255)
 timmy.shape("turtle")
-timmy.pensize(10)
+# timmy.pensize(10)
 timmy.speed("fastest")
 
-def random_color():
-    return (randint(0,255), randint(0, 255), randint(0, 255))
+# def random_color():
+#     return (randint(0,255), randint(0, 255), randint(0, 255))
 
 
 # # Draw a square
@@ -41,12 +47,35 @@ def random_color():
 #         timmy.forward(50)
 #         timmy.left(angle)
 
-# Turle, Random Walk
-for _ in range(200):
-    timmy.color(random_color())
-    timmy.forward(20)
-    timmy.setheading(choice(directions))
+# # Turle, Random Walk
+# for _ in range(200):
+#     timmy.color(random_color())
+#     timmy.forward(20)
+#     timmy.setheading(choice(directions))
 
+
+# # Spiraograph
+# for _ in range(72):
+#     timmy.color(random_color())
+#     timmy.circle(100)
+#     timmy.setheading(timmy.heading() + 5)
+
+
+# Drawing the Dots
+timmy.penup()
+timmy.setheading(225)
+timmy.forward(300)
+timmy.setheading(0)
+for _ in range(10):
+
+    for _ in range(10):
+        timmy.setheading(0)    
+        timmy.dot(20, choice(rgb_colors))
+        timmy.forward(50)
+        timmy.setheading(90)
+    timmy.forward(50)
+    timmy.setheading(180)
+    timmy.forward(500)
 
 
 screen = Screen()
