ee5e956ff173205962654189029e144eb98ca16a
fuzzygwalchmei <marc.falzon@gmail.com>
2020-12-13 13:16:10 +1100

    udemy 100 days of code day 18 ex 3 random colours

--- 18-exercises.py
+++ 18-exercises.py
@@ -1,10 +1,19 @@
 #! /bin/python3 
 # 
-from turtle import Turtle, Screen
+from random import choice, randint
+from turtle import Turtle, Screen, colormode
+
+directions = [0, 90, 180, 270]
 
 timmy = Turtle()
+colormode(255)
 timmy.shape("turtle")
-timmy.color("orange")
+timmy.pensize(10)
+timmy.speed("fastest")
+
+def random_color():
+    return (randint(0,255), randint(0, 255), randint(0, 255))
+
 
 # # Draw a square
 # for _ in range(4):
@@ -18,20 +27,25 @@ timmy.color("orange")
 #     timmy.forward(20)
 #     timmy.pendown()
 
-# Draw multiple shapes
-shapes = {"triangle":{"sides":3, "color":"red"},
-        "square":{"sides":4, "color":"blue"},
-        "pentagon":{"sides":5, "color":"green"}}
+# # Draw multiple shapes
+# shapes = {"triangle":{"sides":3, "color":"red"},
+#         "square":{"sides":4, "color":"blue"},
+#         "pentagon":{"sides":5, "color":"green"}}
 
-for shape in shapes:
-    timmy.color(shapes[shape]["color"])
-    angle = 360/shapes[shape]["sides"]
-    sides = shapes[shape]['sides']
+# for shape in shapes:
+#     timmy.color(shapes[shape]["color"])
+#     angle = 360/shapes[shape]["sides"]
+#     sides = shapes[shape]['sides']
 
-    for _ in range(sides):
-        timmy.forward(50)
-        timmy.left(angle)
+#     for _ in range(sides):
+#         timmy.forward(50)
+#         timmy.left(angle)
 
+# Turle, Random Walk
+for _ in range(200):
+    timmy.color(random_color())
+    timmy.forward(20)
+    timmy.setheading(choice(directions))
 
 
 
