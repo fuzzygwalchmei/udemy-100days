851b92960157a15549599b29bca6228335dcad8e
fuzzygwalchmei <marc.falzon@gmail.com>
2020-12-13 12:59:14 +1100

    udemy 100 days of code Day 18 multiple shapes

--- 18-exercises.py
+++ 18-exercises.py
@@ -6,11 +6,31 @@ timmy = Turtle()
 timmy.shape("turtle")
 timmy.color("orange")
 
-# Draw a square
-for _ in range(4):
-    timmy.forward(100)
-    timmy.right(270)
+# # Draw a square
+# for _ in range(4):
+#     timmy.forward(100)
+#     timmy.right(270)
 
+# # Draw a dashed line
+# for _ in range(10):
+#     timmy.forward(20)
+#     timmy.penup()
+#     timmy.forward(20)
+#     timmy.pendown()
+
+# Draw multiple shapes
+shapes = {"triangle":{"sides":3, "color":"red"},
+        "square":{"sides":4, "color":"blue"},
+        "pentagon":{"sides":5, "color":"green"}}
+
+for shape in shapes:
+    timmy.color(shapes[shape]["color"])
+    angle = 360/shapes[shape]["sides"]
+    sides = shapes[shape]['sides']
+
+    for _ in range(sides):
+        timmy.forward(50)
+        timmy.left(angle)
 
 
 
