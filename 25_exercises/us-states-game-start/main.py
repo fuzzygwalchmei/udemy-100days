c7458e96940d9cb4e8f536abca2178ea8f281180
fuzzygwalchmei <marc.falzon@gmail.com>
2020-12-20 13:59:03 +1100

    udemy 100 days of code day 25 - finished

--- 25_exercises/us-states-game-start/main.py
+++ 25_exercises/us-states-game-start/main.py
@@ -13,9 +13,30 @@ turtle.shape(image)
 coords = csv.reader("50_states.csv")
 df = pd.read_csv("50_states.csv")
 
-guess_state = screen.textinput(title="Guess the state!", prompt="Whats another states name?: ").capitalize()
 
-print(df[df.state == guess_state])
+
+all_states = df.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    guess_state = screen.textinput(title="Guess the state!", prompt="Whats another states name?: ").title()
+
+    if guess_state == 'Quit':
+        diff = set(all_states).difference(set(guessed_states))
+        missed = pd.DataFrame(set(all_states).difference(set(guessed_states)))
+        missed.to_csv("missed_states.csv")
+        break
+
+    elif guess_state in all_states:
+        guessed_states.append(guess_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = df[df.state == guess_state]
+        df[df.state == guess_state]['guessed'] = True
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
+
 
 
 def get_mouse_click_coor(x, y):
