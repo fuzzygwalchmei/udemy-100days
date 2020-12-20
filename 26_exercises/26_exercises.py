70509c9e8f69eb3b7676053e978aeb7307e700b0
fuzzygwalchmei <marc.falzon@gmail.com>
2020-12-21 10:46:52 +1100

    udemy 100 days of code day 26 ex 2

---
+++ 26_exercises/26_exercises.py
@@ -0,0 +1,12 @@
+doubled = [x*2 for x in range(1,5)]
+print(doubled)
+
+# Exercise 1 - Squared Numbers
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+squared_numbers = [x**2 for x in numbers]
+print(squared_numbers)
+
+# Exercises 2 - Even numbers
+even_numbers = [x for x in numbers if x%2==0]
+print(even_numbers)
+
