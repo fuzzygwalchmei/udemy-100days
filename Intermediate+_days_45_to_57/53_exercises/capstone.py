43577c07d563cf8848b93085ada3b1e96f487312
fuzzygwalchmei <marc.falzon@gmail.com>
2021-01-17 13:27:21 +1100

    udemy 100 days of code day 53 web capstone

---
+++ Intermediate+_days_45_to_57/53_exercises/capstone.py
@@ -0,0 +1,53 @@
+import os
+import requests
+from requests.api import get
+import requests_cache
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+from pathlib import Path
+from time import sleep
+
+load_dotenv()
+
+
+FORM_URL = os.getenv('form_url')
+
+
+def write_to_sheet(addr, price, website):
+    home = Path.home()
+    driver = webdriver.Chrome(f"{home}/Code/chromedriver")
+    driver.get(FORM_URL)
+    sleep(2)
+    addr_entry = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    addr_entry.send_keys(addr)
+    price_entry = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    price_entry.send_keys(price)
+    link_entry = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    link_entry.send_keys(website)
+    submit_button = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div')
+    submit_button.click()
+    
+    driver.quit()
+
+
+
+
+def get_properties():
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"}
+    resp = requests.get("https://www.zillow.com/homes/Salinas,-CA_rb/", headers=headers)
+    resp.raise_for_status()
+    page = BeautifulSoup(resp.text, 'html.parser')
+
+
+    listings = page.find_all("div", class_="list-card-info")
+    for list in listings[:2]:
+        addr = list.find(class_='list-card-addr').get_text()
+        price = list.find(class_='list-card-price').get_text()
+        website = list.find(class_='list-card-link')['href']
+
+        write_to_sheet(addr, price, website)
+
+
+get_properties()
