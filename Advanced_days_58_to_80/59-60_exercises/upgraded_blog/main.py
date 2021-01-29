c61046cbef0361838a9c39558d6b05f0213ff212
fuzzygwalchmei <marc.falzon@gmail.com>
2021-01-29 20:49:13 +1100

    udemy 100 days of code day 61 flask forms

--- Advanced_days_58_to_80/59-60_exercises/upgraded_blog/main.py
+++ Advanced_days_58_to_80/59-60_exercises/upgraded_blog/main.py
@@ -1,7 +1,28 @@
 from flask import Flask, render_template, request   
 import requests
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, Email
+from flask_wtf.csrf import CsrfProtect
+
+from flask_bootstrap import Bootstrap
+
+
+
+class Login(FlaskForm):
+    email = EmailField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField(label="Submit")
+
+
+
 app = Flask(__name__)
+
+csrf = CsrfProtect(app)
+app.secret_key = "some rando string"
+Bootstrap(app)
 
 blog_endpoint = 'https://api.npoint.io/43644ec4f0013682fc0d'
 posts = requests.get(blog_endpoint).json()
@@ -40,7 +61,16 @@ def submit():
         return render_template('form-entry.html')
     return render_template('contact.html')
     
-
+@app.route('/login', methods=["GET","POST"])
+def login():
+    success = "Pending"
+    login_form = Login()
+    if login_form.validate_on_submit():
+        if login_form.email.data == 'admin@email.com' and login_form.password.data == '123456':
+            success = "Success"
+        else:
+            success = "Failure"
+    return render_template('login.html', form=login_form, success = success)
 
 if __name__ == "__main__":
     app.run(debug=True)
