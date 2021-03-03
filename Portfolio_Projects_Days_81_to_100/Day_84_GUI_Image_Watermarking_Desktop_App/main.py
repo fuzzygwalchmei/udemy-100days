94ba90bd1e3dda48e8d97660322f4da60e0ac5a4
fuzzygwalchmei <marc.falzon@gmail.com>
2021-03-03 20:19:04 +1100

    udemy 100 days of code day 84 Image Watermark app building

--- Portfolio_Projects_Days_81_to_100/Day_84_GUI_Image_Watermarking_Desktop_App/main.py
+++ Portfolio_Projects_Days_81_to_100/Day_84_GUI_Image_Watermarking_Desktop_App/main.py
@@ -1,8 +1,19 @@
 from tkinter import Tk, Label, Entry, Button, filedialog
+from PIL import Image, ImageDraw, ImageFont
 
 app = Tk()
 
 app.title("Image Watermarker")
+
+
+def transparency():
+    base_img = Image.open(ent_file)
+
+    working_img = ImageDraw.Draw(base_img)
+    black = (3, 8, 12)
+    working_img.text((0,0), ent_text.get(), fill=black)
+    base_img.save('~/test_img.jpg')
+
 
 
 def callback():
@@ -12,11 +23,15 @@ def callback():
 lbl_file = Label(text='Select File: ')
 ent_file = Label()
 btn_file = Button(text='Choose File', command=callback)
-btn_submit = Button(text='Submit')
+lbl_text = Label(text="Text for watermark")
+ent_text = Entry()
+
+btn_submit = Button(text='Submit', command=transparency)
 
 lbl_file.pack()
 ent_file.pack()
 btn_file.pack()
+ent_text.pack()
 btn_submit.pack()
 
 
