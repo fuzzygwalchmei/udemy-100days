4206800d56114d5a55a69c72e89e93e46204d6f5
fuzzygwalchmei <marc.falzon@gmail.com>
2021-03-05 19:09:07 +1100

    udemy 100 days of code day 85 GUI typing speed test:  planning

---
+++ Portfolio_Projects_Days_81_to_100/Day_85_GUI_Typing_Speed_Test/main.py
@@ -0,0 +1,25 @@
+from tkinter import Tk, Label, Entry, Button
+from timeit import Timer
+from tkinter.constants import WORD
+
+
+# import words from a file
+
+
+# GUI
+
+# Label to display words
+# Highlight current word
+# matching character to character, no reversed
+
+# try again button
+# something like speedtest?
+
+
+
+# FEATURES
+
+# words per minute
+# accuracy
+# mistakes
+# difficulty levels
