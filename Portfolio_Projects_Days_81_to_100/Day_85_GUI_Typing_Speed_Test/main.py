0a563f737e2bf0846ba547f2f1da331c63da1ce0
fuzzygwalchmei <marc.falzon@gmail.com>
2021-03-06 13:59:51 +1100

    udemy 100 days of code  day 85 Typing Tester continued

--- Portfolio_Projects_Days_81_to_100/Day_85_GUI_Typing_Speed_Test/main.py
+++ Portfolio_Projects_Days_81_to_100/Day_85_GUI_Typing_Speed_Test/main.py
@@ -1,6 +1,6 @@
 from tkinter import Tk, Label, Entry, Button
-from timeit import Timer
-from tkinter.constants import WORD
+from timeit import timeit
+from typing import Collection
 
 
 # import words from a file
@@ -8,6 +8,15 @@ from tkinter.constants import WORD
 
 # GUI
 
+app = Tk()
+
+
+lbl_display = Label()
+
+ent_text = Entry()
+btn_start = Button(text="Start")
+btn_finish = Button(text="Finish")
+btn_restart = Button(text="Restart")
 # Label to display words
 # Highlight current word
 # matching character to character, no reversed
@@ -15,7 +24,11 @@ from tkinter.constants import WORD
 # try again button
 # something like speedtest?
 
-
+lbl_display.grid(row=0, column=0, columnspan=3)
+ent_text.grid(row=1, column=0, columnspan=3)
+btn_start.grid(row=2, column=0)
+btn_finish.grid(row=2, column=1)
+btn_restart.grid(row=2, column=2)
 
 # FEATURES
 
@@ -23,3 +36,10 @@ from tkinter.constants import WORD
 # accuracy
 # mistakes
 # difficulty levels
+
+
+
+
+
+if __name__ == "__main__":
+    app.mainloop()
